0b46b2539b8006407294d2388d463d5c949ef214
github username <github email address>
2018-12-28 09:33:55 -0500

    Add NavLink and preliminary NavBar component.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 // react-redux is a Redux binding for React. It’s a small library for connecting Redux
 // and React in an efficient way.
 // To start off connecting Redux with React we’re going to use Provider.
@@ -22,10 +22,35 @@ import EntryCard from './components/EntryCard';
 import * as serviceWorker from './serviceWorker';
 import store from './store.js'
 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+  </div>;
+
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <React.Fragment>
+        <Navbar />
         <Route path="/" component={App} />
       </React.Fragment>
     </Router>
